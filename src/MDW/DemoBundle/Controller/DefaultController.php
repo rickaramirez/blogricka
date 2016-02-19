6b723485508a5d931bc8ab632519d0ee0f8f1fb6
Ricardo Ramirez <rickaramirez@192.168.0.106>
2016-02-18 23:23:56 -0600

    Segundo ejercicio terminado

--- src/MDW/DemoBundle/Controller/DefaultController.php
+++ src/MDW/DemoBundle/Controller/DefaultController.php
@@ -14,10 +14,28 @@ class DefaultController extends Controller {
         //-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
         $articulos = array(
             array('id' => 1, 'title' => 'Articulo numero 1', 'created' => '2011-01-01'),
-            array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-01'),
-            array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-01'),
+            array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-02'),
+            array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-03'),
         );
         return $this->render('MDWDemoBundle:Default:articulos.html.twig', array('articulos' => $articulos));
     }
 
+    public function articuloAction($id) {
+        //-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
+        $articulos = array(
+            array('id' => 1, 'title' => 'Articulo numero 1', 'created' => '2011-01-01'),
+            array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-02'),
+            array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-03'),
+        );
+        //-- Buscamos dentro del array el ID solicitado 
+        $articuloSeleccionado = null;
+        foreach ($articulos as $articulo) {
+            if ($articulo['id'] == $id) {
+                $articuloSeleccionado = $articulo;
+                break;
+            }
+        }
+        //-- Invocamos a nuestra nueva plantilla, pasando los datos return 
+        return $this->render('MDWDemoBundle:Default:articulo.html.twig', array('articulo' => $articuloSeleccionado));
+    }
 }
